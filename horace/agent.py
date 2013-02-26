ad0538a510b7cd4f965dac7bbb26a9785f13bc33
Lawrence Carvalho <lawrence@nodetraveller.com>
2013-02-26 22:38:23 +0000

    Cleaned up toAt() code

--- horace/agent.py
+++ horace/agent.py
@@ -2,10 +2,6 @@ from horace.exceptions import NotAtPageException
 
 
 class Agent(object):
-    def __new__(cls, *p, **k):
-        if not '_the_instance' in cls.__dict__:
-            cls._the_instance = object.__new__(cls)
-        return cls._the_instance
 
     def __init__(self, driver):
         self._driver = driver
@@ -21,14 +17,12 @@ class Agent(object):
         self._driver.get(Page.url)
 
     def at(self, Page):
-        if not self._currentPage:
-            self._currentPage = Page(self._driver)
+        self._currentPage = Page(self._driver)
         title = self._driver.title
         if title != self._currentPage.title:
             raise NotAtPageException(self._currentPage.title, title)
 
     def toAt(self, Page):
-        self._currentPage = Page(self._driver)
         self.to(Page)
         self.at(Page)
 
